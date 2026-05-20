d4370ca12fd48775ad4464dea33fb6779e450cf8
Pablo <pablfranca@gmail.com>
2026-05-20 13:11:50 -0300

    "Adição de estrutura de projeto"

---
+++ src/main/java/com/ucook/api/UCookApiApplication.java
@@ -0,0 +1,13 @@
+package com.ucook.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UCookApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UCookApiApplication.class, args);
+    }
+
+}
